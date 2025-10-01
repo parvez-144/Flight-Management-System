f9fb16ec8fd809d73f89cb4f799653d1a9e09ad5
parvez-144 <parvezrazasiddiqui444@gmail.com>
2025-10-01 16:39:18 +0530

    created app.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,9 @@
-const express=require('express');
+const app=require('./app');
 const dotenv=require('dotenv');
 
 dotenv.config();
 const PORT=process.env.PORT || 3000;
-const app=express();
 
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
