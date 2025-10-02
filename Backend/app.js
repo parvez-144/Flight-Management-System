66854d360a0115acbd409b522dd47224ee535b62
parvez-144 <parvezrazasiddiqui444@gmail.com>
2025-10-02 16:44:58 +0530

    connected to MongoDB and added necessary dependencies for the backend.

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./src/config/db');
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+connectDB();
+
 
 
 module.exports = app;
